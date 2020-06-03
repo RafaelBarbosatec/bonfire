f4c3bc6b8fedc030bb33e55b137b7fd3d399d550
Gabriel Rezende <gabrielcdrezende@gmail.com>
2020-06-02 22:30:32 -0300

    remove collision and grid from example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -61,8 +61,6 @@ class Game extends StatelessWidget implements GameListener {
       background: BackgroundColorGame(Colors.blueGrey[900]),
       gameController: _controller..setListener(this),
       lightingColorGame: Colors.black.withOpacity(0.5),
-      constructionMode: true,
-      showCollisionArea: true,
     );
   }
 
