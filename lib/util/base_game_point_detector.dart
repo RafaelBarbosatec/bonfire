a61b280096b8276594f8b8736915eef4cd806d6c
Rafael  Barbosa - Sciensa <rafael.sciensa@MacBook-Pro-de-adminisrador.local>
2020-06-07 11:15:18 -0300

    remove save

--- lib/util/base_game_point_detector.dart
+++ lib/util/base_game_point_detector.dart
@@ -129,7 +129,6 @@ abstract class BaseGamePointerDetector extends Game with PointerDetector {
     }
 
     if (!comp.isHud()) {
-      canvas.save();
       canvas.translate(-gameCamera.position.x, -gameCamera.position.y);
     }
     comp.render(canvas);
