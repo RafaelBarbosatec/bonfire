d72350095e9a96ffc6727f3341da1e0d4eedd5c1
rafaelbarbosatec <rafaelbarbosatec@gmail.com>
2020-12-02 22:06:55 -0300

    ajustes

--- lib/util/collision/collision.dart
+++ lib/util/collision/collision.dart
@@ -5,8 +5,7 @@ class Collision {
   double width;
   final Offset align;
 
-  Collision(
-      {this.height = 0.0, this.width = 0.0, this.align = const Offset(0, 0)});
+  Collision({this.height = 0.0, this.width = 0.0, this.align = const Offset(0, 0)});
 
   Collision.fromSize(double size, {this.align = const Offset(0, 0)}) {
     width = size;
