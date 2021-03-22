155326250c70f029b5d1dcd9b162e9af399410e7
rafaelbarbosatec <rafaelbarbosatec@gmail.com>
2021-03-22 16:57:36 -0300

    ajuste export

--- lib/bonfire.dart
+++ lib/bonfire.dart
@@ -22,7 +22,6 @@ export 'package:bonfire/lighting/lighting_config.dart';
 export 'package:bonfire/map/map_game.dart';
 export 'package:bonfire/map/map_world.dart';
 export 'package:bonfire/map/tile/tile.dart';
-export 'package:bonfire/map/tile/tile_animated_with_collision.dart';
 export 'package:bonfire/map/tile/tile_with_collision.dart';
 export 'package:bonfire/objects/animated_follower_object.dart';
 export 'package:bonfire/objects/animated_object.dart';
