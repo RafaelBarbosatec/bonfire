b03d01c6f4a32f25afae5a67cb397cb43d7cae0c
rafaelbarbosatec <rafaelbarbosatec@gmail.com>
2021-03-30 17:03:18 -0300

    add ClipRRect

--- lib/base/custom_game_widget.dart
+++ lib/base/custom_game_widget.dart
@@ -9,22 +9,24 @@ class CustomGameWidget<T extends Game> extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Listener(
-      onPointerDown: game is PointerDetector
-          ? (PointerDownEvent d) => (game as PointerDetector).onPointerDown(d)
-          : null,
-      onPointerMove: game is PointerDetector
-          ? (PointerMoveEvent d) => (game as PointerDetector).onPointerMove(d)
-          : null,
-      onPointerUp: game is PointerDetector
-          ? (PointerUpEvent d) => (game as PointerDetector).onPointerUp(d)
-          : null,
-      onPointerCancel: game is PointerDetector
-          ? (PointerCancelEvent d) =>
-              (game as PointerDetector).onPointerCancel(d)
-          : null,
-      child: GameWidget(
-        game: game,
+    return ClipRRect(
+      child: Listener(
+        onPointerDown: game is PointerDetector
+            ? (PointerDownEvent d) => (game as PointerDetector).onPointerDown(d)
+            : null,
+        onPointerMove: game is PointerDetector
+            ? (PointerMoveEvent d) => (game as PointerDetector).onPointerMove(d)
+            : null,
+        onPointerUp: game is PointerDetector
+            ? (PointerUpEvent d) => (game as PointerDetector).onPointerUp(d)
+            : null,
+        onPointerCancel: game is PointerDetector
+            ? (PointerCancelEvent d) =>
+                (game as PointerDetector).onPointerCancel(d)
+            : null,
+        child: GameWidget(
+          game: game,
+        ),
       ),
     );
   }
