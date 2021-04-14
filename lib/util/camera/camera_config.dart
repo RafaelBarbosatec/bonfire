625427540f6e8d334149099d5b8c54a39d3f4006
rafaelbarbosatec <rafaelbarbosatec@gmail.com>
2021-04-14 11:40:22 -0300

    adiciona comentarios

--- lib/util/camera/camera_config.dart
+++ lib/util/camera/camera_config.dart
@@ -2,10 +2,18 @@ import 'dart:ui';
 
 import 'package:bonfire/base/game_component.dart';
 
+/// Class use to configure camera behavior.
 class CameraConfig {
+  ///Player movement window before the camera moves
   Size sizeMovementWindow;
+
+  /// When this true the camera remains within the map area
   bool moveOnlyMapArea;
+
+  /// camera zoom configurarion. default: 1
   double zoom;
+
+  /// Component that the camera will focus on / follow
   GameComponent? target;
 
   CameraConfig({
