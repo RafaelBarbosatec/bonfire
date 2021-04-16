f7dbee33573bd9d753b5f70a91d14a45cef29273
rafaelbarbosatec <rafaelbarbosatec@gmail.com>
2021-04-16 14:57:34 -0300

    add export flame/geometry

--- lib/bonfire.dart
+++ lib/bonfire.dart
@@ -74,5 +74,6 @@ export 'package:flame/components.dart'
         JoystickActionAlign,
         ActionEvent;
 export 'package:flame/flame.dart';
+export 'package:flame/geometry.dart';
 export 'package:flame/sprite.dart';
 export 'package:flame/widgets.dart' hide NineTileBox;
