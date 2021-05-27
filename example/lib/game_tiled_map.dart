8a1f0cab59c7b9046b1224d4dbe2f836e775f00a
rafaelbarbosatec <rafaelbarbosatec@gmail.com>
2021-05-27 18:12:13 -0300

    remove test

--- example/lib/game_tiled_map.dart
+++ example/lib/game_tiled_map.dart
@@ -73,7 +73,6 @@ class GameTiledMap extends StatelessWidget {
           ),
           background: BackgroundColorGame(Colors.blueGrey[900]!),
           lightingColorGame: Colors.black.withOpacity(0.7),
-          showCollisionArea: true,
         );
       },
     );
