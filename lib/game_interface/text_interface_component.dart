253bbe0f880bc5c67cf9e88a3a5a38a80d9966be
rafaelbarbosatec <rafaelbarbosatec@gmail.com>
2021-06-22 16:02:14 -0300

    remove width and height in TextInterfaceComponent

--- lib/game_interface/text_interface_component.dart
+++ lib/game_interface/text_interface_component.dart
@@ -15,15 +15,13 @@ class TextInterfaceComponent extends InterfaceComponent {
     required int id,
     required Vector2 position,
     this.text = '',
-    double width = 0,
-    double height = 0,
     ValueChanged<bool>? onTapComponent,
     TextPaintConfig? textConfig,
   }) : super(
           id: id,
           position: position,
-          width: width,
-          height: height,
+          width: 0.0,
+          height: 0.0,
           onTapComponent: onTapComponent,
         ) {
     this.textConfig = TextPaint(config: textConfig ?? TextPaintConfig());
