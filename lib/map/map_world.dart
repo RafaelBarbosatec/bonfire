4469baee0085dd94c65baaefef573a5fc911bfe9
rafaelbarbosatec <rafaelbarbosatec@gmail.com>
2021-09-01 15:37:01 -0300

    adds lots to build tiles

--- lib/map/map_world.dart
+++ lib/map/map_world.dart
@@ -14,12 +14,16 @@ import 'package:flutter/material.dart';
 import 'map_assets_manager.dart';
 
 class MapWorld extends MapGame {
+  static const int _LOT_BUILD_TILE = 100;
   Vector2 lastCamera = Vector2.zero();
   double lastMinorZoom = 1.0;
   Vector2? lastSizeScreen;
   List<ObjectCollision> _tilesCollisions = List.empty();
   List<ObjectCollision> _tilesVisibleCollisions = List.empty();
   List<Tile> _tilesToRemove = [];
+  List<Tile> _tilesToUpdate = [];
+  List<TileModel> _visibleTileModel = [];
+  int _indexBuildTile = -1;
 
   List<Offset> _linePath = [];
   Paint _paintPath = Paint()
@@ -47,9 +51,11 @@ class MapWorld extends MapGame {
 
   @override
   void update(double t) {
-    if (_checkNeedUpdateTiles()) {
+    if (_indexBuildTile == -1 && _checkNeedUpdateTiles()) {
       scheduleMicrotask(_updateTilesToRender);
     }
+
+    _buildTilesLot();
 
     for (var tile in children) {
       tile.update(t);
@@ -61,14 +67,36 @@ class MapWorld extends MapGame {
     _verifyRemoveTiles();
   }
 
+  void _buildTilesLot() {
+    if (_indexBuildTile == -1) return;
+    int sizeList = _visibleTileModel.length;
+    int countLot = (sizeList / _LOT_BUILD_TILE).ceil();
+    int start = _LOT_BUILD_TILE * _indexBuildTile;
+    int end = start + _LOT_BUILD_TILE;
+    if (end > sizeList) {
+      end = sizeList;
+    }
+    var visibleTiles = _visibleTileModel.sublist(start, end);
+    _tilesToUpdate.addAll(_buildTiles(visibleTiles));
+    _indexBuildTile++;
+
+    if (_indexBuildTile >= countLot) {
+      children = _tilesToUpdate.toList();
+      _findVisibleCollisions();
+      _tilesToUpdate.clear();
+      _visibleTileModel.clear();
+      _indexBuildTile = -1;
+    }
+  }
+
   void _updateTilesToRender() {
     final tileSize = tiles.first.width;
     final rectCamera = gameRef.camera.cameraRectWithSpacing;
-    final visibleTiles = quadTree?.query(
-      rectCamera.getRectangleByTileSize(tileSize),
-    );
-    children = _buildTiles(visibleTiles ?? []);
-    _findVisibleCollisions();
+    _visibleTileModel = quadTree?.query(
+          rectCamera.getRectangleByTileSize(tileSize),
+        ) ??
+        [];
+    _indexBuildTile = 0;
   }
 
   @override
