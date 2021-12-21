844f4cf2cec09b97e70f940f2906fc8884756be4
rafaelbarbosatec <rafaelbarbosatec@gmail.com>
2021-12-21 19:24:29 -0300

    remove not used code

--- lib/base/base_game.dart
+++ lib/base/base_game.dart
@@ -72,68 +72,6 @@ abstract class BaseGame extends FlameGame with FPSCounter, PointerDetector {
     }
   }
 
-  // /// This implementation of render basically calls [renderComponent] for every component, making sure the canvas is reset for each one.
-  // ///
-  // /// You can override it further to add more custom behaviour.
-  // /// Beware of however you are rendering components if not using this; you must be careful to save and restore the canvas to avoid components messing up with each other.
-  // @override
-  // // ignore: must_call_super
-  // void render(Canvas canvas) {
-  //   if (!hasLayout) return;
-  //   canvas.save();
-  //
-  //   canvas.translate(size.x / 2, size.y / 2);
-  //   canvas.scale(camera.config.zoom);
-  //   canvas.rotate(camera.config.angle);
-  //   canvas.translate(-camera.position.dx, -camera.position.dy);
-  //
-  //   for (final comp in children) {
-  //     renderComponent(canvas, comp);
-  //   }
-  //
-  //   canvas.restore();
-  // }
-  //
-  // /// This renders a single component obeying BaseGame rules.
-  // ///
-  // /// It translates the camera unless hud, call the render method and restore the canvas.
-  // /// This makes sure the canvas is not messed up by one component and all components render independently.
-  // void renderComponent(Canvas canvas, Component comp) {
-  //   if (comp is GameComponent) {
-  //     if (!comp.isHud && !comp.isVisible) return;
-  //   }
-  //
-  //   canvas.save();
-  //
-  //   if (comp.isHud) {
-  //     canvas.translate(camera.position.dx, camera.position.dy);
-  //     canvas.scale(1 / camera.config.zoom);
-  //     canvas.rotate(-1 * camera.config.angle);
-  //     canvas.translate(-size.x / 2, -size.y / 2);
-  //   }
-  //
-  //   comp.renderTree(canvas);
-  //
-  //   if (debugMode) {
-  //     comp.renderDebugMode(canvas);
-  //   }
-  //
-  //   canvas.restore();
-  // }
-  //
-  // /// This implementation of update updates every component in the list.
-  // ///
-  // /// It also actually adds the components that were added by the [addLater] method, and remove those that are marked for destruction via the [Component.destroy] method.
-  // /// You can override it further to add more custom behaviour.
-  // @override
-  // void update(double dt) {
-  //   super.update(dt);
-  //   camera.update(dt);
-  //   if (parent == null) {
-  //     super.updateTree(dt, callOwnUpdate: false);
-  //   }
-  // }
-
   /// Verify if the Component contain gestures.
   bool _hasGesture(Component c) {
     return ((c is GameComponent && c.isVisible) || c.isHud) &&
@@ -147,13 +85,5 @@ abstract class BaseGame extends FlameGame with FPSCounter, PointerDetector {
       children.rebalanceAll();
       _highestPriority = children.last.priority;
     }
-  }
-
-  @override
-  void onGameResize(Vector2 size) {
-    for (final child in children) {
-      child.onGameResize(size);
-    }
-    super.onGameResize(size);
   }
 }
