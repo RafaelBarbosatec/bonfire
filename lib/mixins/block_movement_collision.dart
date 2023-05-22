d7f6984ce8c41ea3e301845f5003536d10643ed2
rafaelbarbsoatec <rafaelbarbosatec@gmail.com>
2023-05-22 13:25:16 -0300

    ajustes

--- lib/mixins/block_movement_collision.dart
+++ lib/mixins/block_movement_collision.dart
@@ -3,14 +3,18 @@ import 'package:bonfire/bonfire.dart';
 /// Mixin responsible for adding collision
 mixin BlockMovementCollision on Movement {
   bool onBlockMovement(
-      Set<Vector2> intersectionPoints, PositionComponent other) {
+    Set<Vector2> intersectionPoints,
+    GameComponent other,
+  ) {
     return true;
   }
 
   @override
   void onCollision(Set<Vector2> intersectionPoints, PositionComponent other) {
     bool stopOtherMovement = true;
-    bool stopMovement = onBlockMovement(intersectionPoints, other);
+    bool stopMovement = other is GameComponent
+        ? onBlockMovement(intersectionPoints, other)
+        : true;
     if (stopMovement && other is BlockMovementCollision) {
       stopOtherMovement = other.onBlockMovement(intersectionPoints, this);
     }
