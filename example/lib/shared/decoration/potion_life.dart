d22064a80fd95dc5ad40eed172772e2438712729
rafaelbarbsoatec <rafaelbarbosatec@gmail.com>
2023-08-20 00:47:37 -0300

    remove test

--- example/lib/shared/decoration/potion_life.dart
+++ example/lib/shared/decoration/potion_life.dart
@@ -2,8 +2,7 @@ import 'package:bonfire/bonfire.dart';
 import 'package:example/manual_map/dungeon_map.dart';
 import 'package:example/shared/util/common_sprite_sheet.dart';
 
-class PotionLife extends GameDecoration
-    with Sensor<Player>, Movement, BlockMovementCollision, BouncingObject {
+class PotionLife extends GameDecoration with Sensor<Player>, Movement {
   final double life;
   double _lifeDistributed = 0;
 
@@ -28,11 +27,5 @@ class PotionLife extends GameDecoration
     );
     removeFromParent();
     super.onContact(component);
-  }
-
-  @override
-  void onMount() {
-    moveUpLeft();
-    super.onMount();
   }
 }
