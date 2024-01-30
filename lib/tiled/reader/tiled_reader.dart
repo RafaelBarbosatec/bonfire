ff77ab97df0330a62e13a9dc6c1823894738babe
rafaelbarbsoatec <rafaelbarbosatec@gmail.com>
2024-01-30 17:45:44 -0300

    adds header in  TiledNetworkReader

--- lib/tiled/reader/tiled_reader.dart
+++ lib/tiled/reader/tiled_reader.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:bonfire/tiled/cache_provider/tiled_cache_provider.dart';
 import 'package:bonfire/tiled/reader/tiled_asset_reader.dart';
 import 'package:bonfire/tiled/reader/tiled_network_reader.dart';
 import 'package:tiledjsonreader/map/tiled_map.dart';
@@ -13,7 +14,15 @@ abstract class TiledReader {
     return TiledAssetReader(asset: asset);
   }
 
-  static TiledReader network(Uri uri) {
-    return TiledNetworkReader(uri: uri);
+  static TiledReader network(
+    Uri uri, {
+    TiledCacheProvider? cacheProvider,
+    Map<String, String>? headers,
+  }) {
+    return TiledNetworkReader(
+      uri: uri,
+      cacheProvider: cacheProvider,
+      headers: headers,
+    );
   }
 }
