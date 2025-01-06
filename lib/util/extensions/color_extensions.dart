e43aa8ec0ddb125d261cd41e45e6537bd195c7cc
Rafael <rafaelbarbosatec@gmail.com>
2025-01-06 11:08:54 -0300

    chore: update colors

--- lib/util/extensions/color_extensions.dart
+++ lib/util/extensions/color_extensions.dart
@@ -2,6 +2,7 @@ import 'dart:ui';
 
 extension ColorExtensions on Color {
   Color setOpacity(double opacity) {
-    return withOpacity(opacity);
+    assert(opacity >= 0.0 && opacity <= 1.0);
+    return withAlpha((255.0 * opacity).round());
   }
 }
